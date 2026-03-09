0048974f8c7d44996489c98df682d01a9d4579c3
petrakfaceit-netizen <petrakfaceit@gmail.com>
2026-03-10 01:04:48 +0200

    Refactor clone script and update instructions
    Remove unnecessary commands and add new file instructions.

--- builder/clone.sh
+++ builder/clone.sh
@@ -5,19 +5,7 @@ REPO_NAME=$1
 REPO_URL=$2
 COMMIT_SHA=$3
 
+mkdir -p /repositories
 git clone "$REPO_URL" "/repositories/$REPO_NAME"
 cd "/repositories/$REPO_NAME"
 git reset --hard "$COMMIT_SHA"
-```
-
-4. Нажми **Commit changes**
-
----
-
-Потом создай ещё один файл:
-
-1. **Add file** → **Create new file**
-2. Имя файла: `builder/requirements.txt`
-3. Вставь:
-```
-runpod
